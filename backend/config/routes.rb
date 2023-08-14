165a1efd7e17eb773444cb2c1e9b845e562757f7
rsugaya <rsugaya@llc-noa.jp>
2023-08-14 11:52:26 +0900

    実装

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [ :index, :show, :create, :update, :destroy]
+      # get 'posts/index'
+      # get 'posts/show'
+      # get 'posts/create'
+      # get 'posts/update'
+      # get 'posts/destroy'
+    end
+  end
 end
